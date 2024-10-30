20c1ba2f9d28053a42ab9b7d7ffc4de7c1b86bfa
Thomasfp <115250899+Thomasfp@users.noreply.github.com>
2024-10-30 21:13:00 +0100

    feat: new javascript for cancelButton

---
+++ components/close-button/main.js
@@ -0,0 +1,30 @@
+class CancelButton extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+    }
+
+    connectedCallback() {
+        const template = document.createElement('template');
+        template.innerHTML = `
+            <button>
+                <img src="assets/cancel.png" alt="cancel button">
+            </button>
+        `;
+
+        const style = document.createElement('link');
+        style.setAttribute('rel', 'stylesheet');
+        style.setAttribute('href', 'components/close-button/style.css');
+
+        this.shadowRoot.appendChild(style);
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+        this.shadowRoot.querySelector('button').addEventListener('click', () => {
+            if (this.parentElement) {
+                this.parentElement.style.display = 'none';
+            }
+        });
+    }
+}
+
+customElements.define('cancel-button', CancelButton);
