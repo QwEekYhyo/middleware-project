33dcfd775e95e10440f512896970fa535f4e9e78
Logan Lucas <logan.lucas83@gmail.com>
2024-11-29 12:20:54 +0100

    add very basic http server

---
+++ back/RoutingServer/Program.cs
@@ -0,0 +1,90 @@
+﻿using System.Net;
+using System.Text;
+using System.Web;
+using System.Collections.Specialized;
+
+namespace ServerHTTPListener {
+    internal class Program {
+        private static void Main(string[] args) {
+            // if HttpListener is not supported by the Framework
+            if (!HttpListener.IsSupported) {
+                Console.WriteLine("A more recent Windows version is required to use the HttpListener class.");
+                return;
+            }
+ 
+            // Create a listener.
+            HttpListener listener = new HttpListener();
+
+            // Add the prefixes.
+            if (args.Length != 0) {
+                foreach (string s in args) 
+                    listener.Prefixes.Add(s);
+            } else
+                Console.WriteLine("Syntax error: the call must contain at least one web server url as argument"); 
+
+            listener.Start();
+
+            // get args 
+            foreach (string s in args)
+                Console.WriteLine("Listening for connections on " + s);
+
+            // Trap Ctrl-C on console to exit 
+            Console.CancelKeyPress += delegate {
+                // call methods to close socket and exit
+                listener.Stop();
+                listener.Close();
+                Environment.Exit(0);
+            };
+
+
+            while (true) {
+                // Note: The GetContext method blocks while waiting for a request.
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerRequest request = context.Request;
+
+                string documentContents;
+                using (Stream receiveStream = request.InputStream) {
+                    using (StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8)) {
+                        documentContents = readStream.ReadToEnd();
+                    }
+                }
+                
+                // get url 
+                Console.WriteLine($"Received request for {request.Url}");
+
+                //get url protocol
+                Console.WriteLine(request.Url.Scheme);
+                //get user in url
+                Console.WriteLine(request.Url.UserInfo);
+                //get host in url
+                Console.WriteLine(request.Url.Host);
+                //get port in url
+                Console.WriteLine(request.Url.Port);
+                //get path in url 
+                Console.WriteLine(request.Url.LocalPath);
+
+                //get params un url. After ? and between &
+                NameValueCollection thing = HttpUtility.ParseQueryString(request.Url.Query);
+
+                string responseString = "{'coucou': 'les amis'}";
+
+                Console.WriteLine(documentContents);
+
+                // Obtain a response object.
+                HttpListenerResponse response = context.Response;
+
+                // Construct a response.
+                
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                // Get a response stream and write the response to it.
+                response.ContentLength64 = buffer.Length;
+                System.IO.Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+                // You must close the output stream.
+                output.Close();
+            }
+            // Httplistener neither stop ... But Ctrl-C do that ...
+            // listener.Stop();
+        }
+    }
+}
